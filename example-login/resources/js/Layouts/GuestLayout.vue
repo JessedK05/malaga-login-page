1fc4ccc4e192eb9cc792f246f3d4598270686c6c
Angel <angelripo5@gmail.com>
2025-01-23 18:38:30 +0100

    Added Register Style

--- example-login/resources/js/Layouts/GuestLayout.vue
+++ example-login/resources/js/Layouts/GuestLayout.vue
@@ -1,3 +1,33 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
 <template>
-    <slot />
+    <!-- <div>
+        <Link href="/">
+            <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+        </Link>
+    </div> -->
+
+    <div class="form-continner">
+        <slot />
+    </div>
 </template>
+
+<style scoped>
+    body {
+        width: 100%;
+    }
+
+    .form-continner {
+        margin: 0;
+        padding: 0;
+
+        width: 100%;
+        
+        background-color: white;
+
+        display: flex;
+    }
+</style>
